28b03f7d3ec6c761e74c16031c55347393644b80
Aliafe Abe <80157852+Atcha-Abe@users.noreply.github.com>
2021-08-28 15:56:48 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register(serviceworker.js).then(registration => {
+  navigator.serviceWorker.register(serviceWorker.js).then(registration => {
     console.log("SW Registered!");
     console.log(registration);
   }).catch(error => {
